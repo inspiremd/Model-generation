4528070131cba5850c2d1532b62b4281a41eed90
David W Wright <dave.william.wright@gmail.com>
2019-07-29 20:19:40 +0100

    Initial attempt at a readable CLI for docking.

---
+++ cli/docking.py
@@ -0,0 +1,20 @@
+"""INSPIRE Docking - Put a ligand (SMILES) in a protein (PDB)
+
+Usage:
+  docking.py -s=<SMILES> -p=<PDB>
+  docking.py (-h | --help)
+  docking.py --version
+
+Options:
+  -h --help     Show this screen.
+  --version     Show version.
+  -s=<SMILES>   Path to file containing SMILES representation of small molecule ligand.
+  -p=<PDB>      Path to PDB file containing protein target for docking.
+
+"""
+from docopt import docopt
+
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__, version='INSPIRE Docking 0.0.1')
+    print(arguments)
