bdb29ca5632ef7ff18ddea8c112c2bd3b6021c71
Adrian Devitt-Lee <adriand1@rhea-login4g.ccs.ornl.gov>
2019-08-30 10:48:23 -0400

    Speed up conformer generation Particularly on molecules that fail.

--- impress_md/conf_gen.py
+++ impress_md/conf_gen.py
@@ -1,12 +1,13 @@
 import sys
 from openeye import oechem, oeomega
 
-def FromMol(mol, isomer=True):
+def FromMol(mol, isomer=True, num_enantiomers=1):
     """
     Generates a set of conformers as an OEMol object
     Inputs:
         mol is an OEMol
         isomers is a boolean controling whether or not the various diasteriomers of a molecule are created
+        num_enantiomers is the allowable number of enantiomers. For all, set to -1
     """
     omegaOpts = oeomega.OEOmegaOptions()
     omega = oeomega.OEOmega(omegaOpts)
@@ -25,12 +26,15 @@ def FromMol(mol, isomer=True):
             ret_code = omega.Build(enantiomer)
             if ret_code == oeomega.OEOmegaReturnCode_Success:
                 out_conf.append(enantiomer)
+                num_enantiomers -= 1
+                if num_enantiomers == 0:
+                    break
             else:
                 oechem.OEThrow.Warning("%s: %s" % (mol.GetTitle(), oeomega.OEGetOmegaError(ret_code)))
 
     return out_conf
 
-def FromString(smiles, isomer=True):
+def FromString(smiles, isomer=True, num_enantiomers=1):
     """
     Generates an set of conformers from a SMILES string
     """
@@ -39,7 +43,7 @@ def FromString(smiles, isomer=True):
         print("SMILES invalid for string", smiles)
         return None
     else:
-        return FromMol(mol,isomer)
+        return FromMol(mol,isomer,num_enantiomers)
 
 # This is clearly not the right way to do it. TODO
 def SelectEnantiomer(mol_list):
