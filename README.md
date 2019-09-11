dcf8b3cf0c3ae3e41d8fc740efee74f6b1626fa7
adriandl <adriandlee@gmail.com>
2019-09-11 13:50:16 +0100

    Commenting code

--- README.md
+++ README.md
@@ -1,29 +1,24 @@
 # Model-generation
-Python scripts to generate an MD-ready model from smiles strings
+Python scripts to generate an MD-ready model from smiles strings and run simple free energy calculations 
 
 There are two command line executables.
 
 ## 0_docking.py
-* Takes a smiles and pdb, generates conformers, docks, scores, and parameterizes the ligand.
+* Takes a smiles and pdb, generates conformers, docks, scores, and parameterizes the ligand. Also writes the protein structure to a separate file (apo).
 * The output is a set of simulation-ready structures (ligand, apo, and complex) and a file called metrics.csv, which has the docking score and associated uncertainties. Most uncertainties are 0 right now. There are other auxiliary files that are saved in the output directory.
 * Dependencies: OpenEye, Ambertools, Ambermini, docopt
 
 ## 1_mmgbsa.py
 * This command should only be run after docking.py. 
-* Takes the path to a directory where the input coordinates and parameters are saved. This should be the output path from the docking.py command.
+* Input is a path to the directory where the input coordinates and parameters are saved. This should be the output path from the docking.py command.
 * Also takes the nanosecond length of the simulation. 0 corresponds to an energy minimization.
 * Output adds to the metrics.csv file
 * Dependencies: OpenMM, numpy, pymbar, docopt
 
 
-To get the first two metrics for a smiles, pick a smiles and call
+To get the three metrics for a smiles, including a 5 ns simulation, pick a smiles and call
 ~~~
 python 0_docking.py -s $SMILES -i "input/receptor.oeb" -o "test" -p
-python 1_mmgbsa.py -p "test" -n 0 
+python 1_mmgbsa.py -p "test" -n 0
+python 1_mmgbsa.py -p "test" -n 5
 ~~~
-
-
-Note
-
-Parameterization is done using OpenEye's AM1BCC charges. 
-Something is going wrong with mmgbsa for -n 1. It runs locally and on Summit's login nodes, but CUDA is not properly recognized when submitted through jsrun.
--- impress_md/mmgbsa.py
+++ impress_md/mmgbsa.py
@@ -2,106 +2,65 @@ import numpy as np
 from pymbar import timeseries
 from math import sqrt
 
+
 def simulate(inpcrd_filenames, prmtop_filenames, niterations=1000, implicit=True):
     """
-    Run simple MM-PBSA like workflow to estimate free energy of binding from enthalpies of interaction.
-    This method can accept either explicit or implicit solvent.
-    .. todo :: Allow user to leave out 'receptor' if desired.
-
-    Parameters
-    ----------
-    inpcrd_filename : dict of str
-        inpcrd_filename[phase] is the Amber inpcrd filename for phase in 'complex', 'ligand', and (optionally) 'receptor'
-    prmtop_filenames : dict of str
-        prmtop_filename[phase] is the Amber prmtop filename for phase in 'complex', 'ligand', and (optionally) 'receptor'
-    niterations : int, optional, default=100
-        Maximum number of iterations to run.
-
-    Returns
-    -------
-    enthalpies: float array
-        A sample of GB enthalpies from the trajectory
+    The program simulates three systems: the ligand alone, protein alone, and complex.
+    Input is a dict of files to the input coordinates (.inpcrd) and parameters (.prmtop) 
+      as well as the number of iterations. One iteration is one picosecond. 
+    Output is a dict of a list of the ennthalpies calculated using mmgbsa for each system.
     """
-    from simtk import unit, openmm
     from simtk.openmm import app
-
-    temperature = 300 * unit.kelvin
-    collision_rate = 1.0 / unit.picoseconds
-    hydrogen_mass = 3.5 * unit.amu
-    timestep = 4.0 * unit.femtoseconds
-    pressure = 1.0 * unit.atmospheres
-
-    if implicit:
-        method = app.CutoffNonPeriodic
-    else:
-        method = app.PME
-
-    # Store thermal energy
-    kT = unit.BOLTZMANN_CONSTANT_kB * temperature
-
-    # TODO: Autodetect nonbondedMethod
-
-    # Termination criteria
-    nsteps_per_iteration = 250 # 1 picosecond
-    # TODO: Use statistical error threshold to drive this to convergence
+    import simtk.openmm as mm
+    from simtk import unit
+    
     phases = inpcrd_filenames.keys()
-
+    nsteps_per_iteration = 500 # 1 picosecond
+    
     enthalpies = dict()
     for phase in phases:
-        # Load Amber system
+        enthalpies[phase] = np.zeros([niterations])
         prmtop = app.AmberPrmtopFile(prmtop_filenames[phase])
         inpcrd = app.AmberInpcrdFile(inpcrd_filenames[phase])
-
-        if not implicit:
-            box = inpcrd.getBoxVectors()
-            context.setPeriodicBoxVectors(a=box[0], b=box[1], c=box[2])
-
-        cutoff = 2*unit.nanometers
-
-        # Create system
-        # Implicit solvent
-        system = prmtop.createSystem(implicitSolvent=app.GBn2,
-                                     nonbondedMethod=method,
-                                     constraints=app.HBonds,
-                                     nonbondedCutoff=cutoff,
-                                     hydrogenMass=hydrogen_mass)
-
-        # Create integrator and context
-        integrator = openmm.LangevinIntegrator(temperature, collision_rate, timestep)
-
-        # platform = openmm.Platform.getPlatformByName('CUDA')
-        simulation = app.Simulation(prmtop.topology, system, integrator)
-        simulation.context.setPositions(inpcrd.getPositions())
         
-        if not implicit:
-            simulation.context.setPeriodicBoxVectors(inpcrd.getPeriodicBoxVectors())
-
-        # Sample and store enthalpies
-        enthalpies[phase] = np.zeros([niterations])
-
-        openmm.LocalEnergyMinimizer.minimize(simulation.context)
-
+        system = prmtop.createSystem(implicitSolvent=app.GBn2, 
+                 nonbondedMethod=app.CutoffNonPeriodic,
+                 nonbondedCutoff=2.0*unit.nanometers, 
+                 constraints=app.HBonds)
+        integrator = mm.LangevinIntegrator(300*unit.kelvin, 1.0/unit.picoseconds, 2.0*unit.femtoseconds)
+        integrator.setConstraintTolerance(0.00001)
+        
+        platform = mm.Platform.getPlatformByName('CUDA')
+        # I'm not sure what the CudaDeviceIndex is about. Austin at 
+        properties = {'CudaPrecision': 'mixed', 'CudaDeviceIndex' : '0'}
+        simulation = app.Simulation(prmtop.topology, system, integrator, platform, properties)
+        simulation.context.setPositions(inpcrd.positions)
+        
+        # Minimize & equilibrate
+        simulation.minimizeEnergy()
+        simulation.context.setVelocitiesToTemperature(300*unit.kelvin)
+        simulation.step(100)
+        
+        # Run simulation
         for iteration in range(niterations):
-            # print("On iteration",iteration,"of",niterations)
-            integrator.step(nsteps_per_iteration)
+            simulation.step(nsteps_per_iteration)
             state = simulation.context.getState(getEnergy=True)
             potential_energy = state.getPotentialEnergy()
-
-            if implicit:
-                enthalpies[phase][iteration] = potential_energy.value_in_unit(unit.kilojoules_per_mole)
-            else:
-                volume = state.getPeriodicBoxVolume()
-                enthalpy = potential_energy + (pressure * volume).in_unit_system(unit.si_unit_system) / unit.mole
-                enthalpies[phase][iteration] = enthalpy.value_in_unit(unit.kilojoules_per_mole)
+            enthalpies[phase][iteration] = potential_energy.value_in_unit(unit.kilojoules_per_mole)
+        del simulation
+        del system
+        del platform
     return enthalpies
+
 
 def subsample(enthalpies):
     """
     Subsamples the enthalpies using John Chodera's code.
-    I want to compare results to a cutoff
-        Returns a smaller array
+    This is probably better than the simple cutoff we normally use.
+    No output -- it modifies the lists directly
     """
     for phase in enthalpies:
+        # Use automatic equilibration detection and pymbar.timeseries to subsample
         [t0, g, Neff_max] = timeseries.detectEquilibration(enthalpies[phase])
         enthalpies[phase] = enthalpies[phase][t0:]
         indices = timeseries.subsampleCorrelatedData(enthalpies[phase], g=g)
@@ -110,20 +69,19 @@ def subsample(enthalpies):
 
 def mmgbsa(enthalpies):
     """
-    DeltaG, errDeltaG : float
-        Estimated free energy of binding (in kT)
-    TODO: it calculates standard error rather than variance
+    Returns DeltaG, errDeltaG : float
+        Estimated free energy of binding
     """
-    # Use automatic equilibration detection and pymbar.timeseries to subsample
     DeltaH = dict()
     varDeltaH = dict()
     errDeltaH = dict()
+    # TODO: it calculates standard error rather than variance
     for phase in enthalpies:
         DeltaH[phase] = enthalpies[phase].mean()
         varDeltaH[phase] = enthalpies[phase].std()**2
         errDeltaH[phase] = varDeltaH[phase]/len(enthalpies[phase])
 
-    # Shouldn't have to use names?
+    # TODO: Shouldn't have to use names?
     try:
         DeltaH['diff'] = 2*DeltaH['complex']
     except:
